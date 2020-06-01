5c2ebf5069bd8b13f71fea75856f3789d5f3a688
Victor Ingman <viregistermail@gmail.com>
2020-06-01 18:14:42 +0200

    added links

--- src/App.js
+++ src/App.js
@@ -32,6 +32,29 @@ const LineList = styled.ul`
 }
 `
 
+const SideNavList = styled.ul`
+  list-style: none;
+  text-align: right;
+  line-height: 150%;
+`
+
+const NavList = styled.ul`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem 1rem 1rem 1rem;
+  text-align: center;
+  & > li {
+    display: inline-block;
+    flex: 1;
+    padding: 1rem;
+    &.selected {
+      border: 2px solid blue;
+    }
+  }
+`
+
 const Title = styled.h1`
   font-family: Verdana, Geneva, Tahoma, sans-serif;
   text-align: center;
@@ -52,28 +75,30 @@ const MaxWidth = styled.div`
   max-width: ${props => props.maxWidth || '1024px'};
 `
 
+const ExtLink = ({ children, ...props }) => <a target='_blank' rel='noopener noreferrer' {...props}>{children}</a>
+
 function App () {
   return (
     <AppWrapper>
       <Side>
         <nav>
-          <ul>
+          <SideNavList>
             <li>
-              <a href=''>source code</a>
+              <ExtLink href='https://github.com/search?q=topic%3Atakt+user%3Axeloader'>source code</ExtLink>
             </li>
             <li>
-              <a href=''>buy me a coffee</a>
+              <ExtLink href='https://www.buymeacoffee.com/xeloader'>buy me a coffee</ExtLink>
             </li>
             <li>
-              <a href=''>my portfolio</a>
+              <ExtLink href='https://ingman.me'>my portfolio</ExtLink>
             </li>
-          </ul>
+          </SideNavList>
         </nav>
       </Side>
       <MaxWidth maxWidth='512px'>
         <Title>TAKT</Title>
-        <P>Boxes that are modular and designed for 3D printing with sustainability in mind. The fragile parts are small and replaceable for waste reduction if something breaks.</P>
-        <P>The boxes are compatible with the <a href='#'>FREKVENS</a> collection from <a href='#'>IKEA</a> and <a href='#'>Teenage Engineering</a>.</P>
+        <P>Building blocks that are modular and designed for 3D printing with sustainability in mind. The fragile parts are small and replaceable for waste reduction if something breaks.</P>
+        <P>The boxes are compatible with the <ExtLink href='https://teenage.engineering/designs/frekvens'>FREKVENS</ExtLink> collection from <ExtLink href='https://about.ikea.com/'>IKEA</ExtLink> and <ExtLink href='https://teenage.engineering'>Teenage Engineering</ExtLink>.</P>
         <LineList>
           <li>3D printable without support</li>
           <li>Modular and connectable to each other</li>
@@ -81,17 +106,17 @@ function App () {
         </LineList>
 
         <nav>
-          <ul>
+          <NavList>
             <li>
               <a href=''>kits</a>
             </li>
-            <li>
+            <li className='selected'>
               <a href=''>pieces</a>
             </li>
             <li>
               <a href=''>photos</a>
             </li>
-          </ul>
+          </NavList>
         </nav>
       </MaxWidth>
       <Grid>
